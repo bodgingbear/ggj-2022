9daa0685ff7cda1f7f565c5cf712b170c81ca3ca
Piotr Szadkowski <offthecode.com>
2022-01-29 22:35:20 +0100

    Remove red rectangle from the bottom right corner - wtf

--- src/scenes/HUDScene.ts
+++ src/scenes/HUDScene.ts
@@ -10,7 +10,5 @@ export class HUDScene extends Phaser.Scene {
   public create(): void {
     const bar = new BladderBar(this);
     this.add.existing(bar.bladderShrinking);
-
-    this.add.rectangle(1280, 720 - 50, 100, 100, 0xff0000);
   }
 }
